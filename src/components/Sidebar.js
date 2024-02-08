4ea0c2ad8335715639cf4fd83aa6bd03d6153026
sabbir421 <sabbiralazim421@gmail.com>
2024-02-08 23:52:53 +0600

    create promo from

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,51 +1,54 @@
-import React from "react";
-import { Sidebar as ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import React, { useState } from "react";
+import { Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { Link } from "react-router-dom";
-import "./sidebar.css";
 
 const Sidebar = () => {
+  const [showPromotionSubMenu, setShowPromotionSubMenu] = useState(false);
+
+  const handlePromotionClick = () => {
+    setShowPromotionSubMenu(!showPromotionSubMenu);
+  };
+
   return (
-    <ProSidebar style={{ backgroundColor: "white", width: "100%", minWidth: "100%" }}>
-      <Menu>
-        <MenuItem>
-          <Link to="/admin">
-            <div className="menu">
-              <div>Promotion</div>
-            </div>
-          </Link>
-          <Menu>
-            <MenuItem>
-              <Link to="/admin/promotion/promocode">
-                <div className="submenu">
-                  <div>Promocode</div>
-                </div>
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/admin/promotion/promocode-list">
-                <div className="submenu">
-                  <div>Promocode List</div>
-                </div>
-              </Link>
-            </MenuItem>
-          </Menu>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/order">
-            <div className="menu">
-              <div>Orders</div>
-            </div>
-          </Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/admin/product">
-            <div className="menu">
-              <div>Products</div>
-            </div>
-          </Link>
-        </MenuItem>
-      </Menu>
-    </ProSidebar>
+    <Menu style={{ backgroundColor: "#fff", width: "250px", minWidth: "250px", height: "100vh", position: "fixed" }}>
+      <MenuItem onClick={handlePromotionClick}>
+        <div style={{ padding: "10px 20px" }}>
+          <div>Promotion</div>
+        </div>
+      </MenuItem>
+      {showPromotionSubMenu && (
+        <SubMenu title="Promotion" defaultOpen={true}>
+          <MenuItem>
+            <Link to="/admin/promotion/promocode">
+              <div style={{ padding: "5px 20px" }}>
+                <div>Promocode</div>
+              </div>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link to="/admin/promotion/promocode-list">
+              <div style={{ padding: "5px 20px" }}>
+                <div>Promocode List</div>
+              </div>
+            </Link>
+          </MenuItem>
+        </SubMenu>
+      )}
+      <MenuItem>
+        <Link to="/order">
+          <div style={{ padding: "10px 20px" }}>
+            <div>Orders</div>
+          </div>
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link to="/admin/product">
+          <div style={{ padding: "10px 20px" }}>
+            <div>Products</div>
+          </div>
+        </Link>
+      </MenuItem>
+    </Menu>
   );
 };
 
--- src/pages/CreatePromo.js
+++ src/pages/CreatePromo.js
@@ -1,13 +1,95 @@
-import React from "react";
-import Layout from "../components/layout";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { TextField, Button, Grid, Typography, Switch } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import { createPromocode } from "./actions/promocodeActions";
 
-const CreatePromo = () => {
+const CreateProduct = () => {
+  const [promocode, setPromocode] = useState("");
+  const [discountRate, setDiscountRate] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [useTime, setUseTime] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSwitch = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      promocode,
+      discountRate,
+      startDate,
+      endDate,
+      useTime,
+      isActive,
+    });
+  };
+
   return (
-    <Layout>
-      <Typography>Add Promo</Typography>
-    </Layout>
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={2} style={{ width: "30%", marginLeft: "35%" }}>
+        <Grid item xs={12}>
+          <Typography variant="h6">Create Promocode</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Promocode"
+            value={promocode}
+            onChange={(e) => setPromocode(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <input
+            type="date"
+            style={{ width: "100%" }}
+            label="Start Date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <input
+            style={{ width: "100%" }}
+            type="date"
+            label="End Date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Discount rate"
+            value={discountRate}
+            onChange={(e) => setDiscountRate(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Use Time"
+            value={useTime}
+            onChange={(e) => setUseTime(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <label>Status</label>
+          <Switch onChange={handleSwitch} />
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
-export default CreatePromo;
+export default CreateProduct;

df60bca150aa64187b8be82227897a31c6eed145
Sabbircoppanet <sabbir.a@coppanet.com>
2024-02-09 05:05:03 +0600

    add feature

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-// export const api = process.env.REACT_APP_END_POINT;
-export const api = "http://localhost:8081";
+export const api = process.env.REACT_APP_END_POINT;
+// export const api = "http://localhost:8081";
